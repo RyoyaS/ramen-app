8a7de3442665d67b9ec167cf6eb01f89364ac92d
Ryoya <ryouya1998.1.21@gmail.com>
2024-09-12 21:11:49 +0900

    足りてない定義追加

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -16,6 +16,7 @@ const Home = () => {
         id: string;
         photo: { pc: { l: string } };
         name: string;
+        address: string;
     }
 
     const [shopList, setShopList] = useState<Shop[]>([]);
